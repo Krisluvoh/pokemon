1e6ab6c8fc3580cb40508bfc817f821ec8de8464
Krisluvoh <krisluvohio@gmail.com>
2023-12-23 18:19:42 -0500

    connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 // Import the express module
+// server.js
+
 const express = require('express');
-
-// Create an instance of the express application
 const app = express();
+const pokemon = require('./pokemon');
 
-// Set the port to 3000
 const port = 3000;
 
-// Set up a route for the home page
 app.get('/', (req, res) => {
   res.send('Welcome to the Pokemon App!');
 });
 
-// Listen on the specified port
+app.get('/pokemon', (req, res) => {
+  res.json(pokemon); // Display the Pokemon data as JSON
+});
+
+// Dynamic route to get Pokemon image based on the number
+app.get('/pokemon/:id', (req, res) => {
+  const { id } = req.params;
+  const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+  res.send(`<img src="${imgUrl}" alt="Pokemon ${id}"/>`);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
